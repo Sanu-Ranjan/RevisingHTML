c4c3d0c22480e9bb7e3abd170c9041c322996662
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-12 14:31:24 +0530

    problem1

---
+++ DOM/practise.js
@@ -0,0 +1,8 @@
+alert("Working");
+
+//exersice 1
+let fruits = document.getElementsByTagName("li");
+fruits[4].style.color = "blue";
+for (let list of fruits) {
+  list.style.fontStyle = "italic";
+}
