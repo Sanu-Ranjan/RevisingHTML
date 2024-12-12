ed530bfab563534c761f5f51069ec9dcc1ad639e
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-12 15:55:58 +0530

    problem2

--- DOM/practise.js
+++ DOM/practise.js
@@ -1,8 +1,19 @@
-alert("Working");
+// 1. Inside the first Div, after the main heading add a sub-heading (h3 tag) "Buy high quality organic fruits online".
 
-//exersice 1
-let fruits = document.getElementsByTagName("li");
-fruits[4].style.color = "blue";
-for (let list of fruits) {
-  list.style.fontStyle = "italic";
-}
+// 2. Make the sub-heading text italic.
+
+// 2. Inside the second Div, before the unordered list add a paragraph tag showing "Total fruits: 4".
+
+// 3. On this paragraph tag set an id of "fruits-total".
+let newh3 = document.createElement("h3");
+newh3.textContent = "Buy high quality organic fruits online";
+newh3.style.fontStyle = "italic";
+document.getElementById("header").append(newh3);
+
+//document.querySelector("div:nth-of-type(2)")
+let unodered = document.querySelector(".fruits");
+console.log(unodered);
+let para = document.createElement("p");
+para.textContent = "Total fruits:4";
+
+document.querySelector("div:nth-of-type(2)").insertBefore(para, unodered);
